cea6487c866dbca6e9ed8b88fa3b599a2cd63be0
Viktoria <vik25@i.ua>
2024-02-04 21:58:46 +0100

    the focus does not go beyond the burger-menu

--- src/components/burgerMenu/burgerMenu.jsx
+++ src/components/burgerMenu/burgerMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ScrollTo } from 'components/ScrollTo';
 import { Close } from 'components/svg/close';
@@ -8,95 +8,106 @@ import { ArrowBurger } from 'components/svg/arrow-burger';
 import css from 'components/burgerMenu/burgerMenu.module.css';
 
 export const BurgerMenu = ({ handleClick }) => {
+  useEffect(() => {
+    const handleKeyDown = event => {
+      const firstElement = document.querySelector(`.${css.closeButton}`);
+      const lastElement = document.querySelector(`.${css.socialInsta}`);
+      const activeElement = document.activeElement;
+      const value = activeElement.getAttribute('value');
+
+      if (value === 'firstButton' && event.key === 'Tab' && event.shiftKey) {
+        event.preventDefault();
+        lastElement && lastElement.focus();
+      } else if (
+        value === 'lastButton' &&
+        event.key === 'Tab' &&
+        !event.shiftKey
+      ) {
+        event.preventDefault();
+        firstElement && firstElement.focus();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <div className={css.burgerMenu}>
       <section className={css.container}>
         <div className={css.buttonWrapper} onClick={handleClick}>
-          <button className={css.closeButton}>
+          <button className={css.closeButton} value="firstButton" autoFocus>
             <Close />
             <span className={css.buttonText}> close</span>
           </button>
         </div>
-        <div className={css.wrapper}>
+        <nav className={css.wrapper}>
           <ul className={css.list}>
-            <li
-              className={css.set}
-              tabIndex="0"
-              role="menuitem"
-              onClick={handleClick}
-            >
+            <li className={css.set} role="menuitem" onClick={handleClick}>
               <ScrollTo
                 target="main"
                 offset={112}
                 className={css.link}
                 ariaLabel='Go to "main"'
               >
-                <p className={css.setText}>Main</p>
-                <ArrowBurger className={css.arrowBurger} />
+                <button className={css.buttons}>
+                  <p className={css.setText}>Main</p>
+                  <ArrowBurger className={css.arrowBurger} />
+                </button>
               </ScrollTo>
             </li>
-            <li
-              className={css.set}
-              tabIndex="0"
-              role="menuitem"
-              onClick={handleClick}
-            >
+            <li className={css.set} role="menuitem" onClick={handleClick}>
               <ScrollTo
                 target="about"
                 offset={112}
                 className={css.link}
                 ariaLabel='Go to "about"'
               >
-                <p className={css.setText}>About</p>
-                <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                <button className={css.buttons}>
+                  <p className={css.setText}>About</p>
+                  <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                </button>
               </ScrollTo>
             </li>
-            <li
-              className={css.set}
-              tabIndex="0"
-              role="menuitem"
-              onClick={handleClick}
-            >
+            <li className={css.set} role="menuitem" onClick={handleClick}>
               <ScrollTo
                 target="cases"
                 offset={112}
                 className={css.link}
                 ariaLabel='Go to "cases"'
               >
-                <p className={css.setText}>Cases</p>
-                <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                <button className={css.buttons}>
+                  <p className={css.setText}>Cases</p>
+                  <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                </button>
               </ScrollTo>
             </li>
-            <li
-              className={css.set}
-              tabIndex="0"
-              role="menuitem"
-              onClick={handleClick}
-            >
+            <li className={css.set} role="menuitem" onClick={handleClick}>
               <ScrollTo
                 target="questions"
                 offset={112}
                 className={css.link}
                 ariaLabel='Go to "questions"'
               >
-                <p className={css.setText}>FAQ</p>
-                <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                <button className={css.buttons}>
+                  <p className={css.setText}>FAQ</p>
+                  <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                </button>
               </ScrollTo>
             </li>
-            <li
-              className={css.set}
-              tabIndex="0"
-              role="menuitem"
-              onClick={handleClick}
-            >
+            <li className={css.set} role="menuitem" onClick={handleClick}>
               <ScrollTo
                 target="contactUs"
                 offset={112}
                 className={css.link}
                 ariaLabel='Go to "contact"'
               >
-                <p className={css.setText}>Contact Us</p>
-                <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                <button className={css.buttons}>
+                  <p className={css.setText}>Contact Us</p>
+                  <ArrowBurger color="rgba(255, 255, 255, 1)" />
+                </button>
               </ScrollTo>
             </li>
           </ul>
@@ -116,11 +127,14 @@ export const BurgerMenu = ({ handleClick }) => {
               rel="noopener noreferrer"
               className={css.socialInsta}
               aria-label="Open our Instagram page"
+              tabIndex="0"
+              role="button"
+              value="lastButton"
             >
               <Instagram />
             </a>
           </div>
-        </div>
+        </nav>
       </section>
     </div>
   );
